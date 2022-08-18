c8be18f456a5027f4f9cd7331c04476a83746d25
cerchiariluiza <luiza.andrade@bee2pay.com>
2022-08-18 02:43:47 -0300

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -10,12 +10,12 @@ terraform {
     }
   }
   required_version = ">= 1.1.0"
-
+  
   cloud {
-    organization = "REPLACE_ME"
+    organization = "cerchiariluiza"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "bee2pay"
     }
   }
 }
